287d50528a2061293bab24e5ea76dd282a45095f
chuokoh <chukwuebukajosh8@gmail.com>
2026-05-13 16:55:59 +0100

    feat: data manipulation

---
+++ file-word-counter/go.mod
@@ -0,0 +1,3 @@
+module fileCounter
+
+go 1.26.1
---
+++ file-word-counter/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+	"unicode/utf8"
+)
+
+func main() {
+	rFile, err := os.Open("sample.txt")
+	if err != nil {
+		fmt.Println("Error: file does not exist", err)
+		return
+	}
+
+	defer rFile.Close()
+
+	scanner := bufio.NewScanner(rFile)
+
+	count, countChar, countStr := 0, 0, 0
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		countChar += utf8.RuneCountInString(line)
+
+		countStr += len(strings.Fields(line))
+
+		//* counting lines
+		count++
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error: reading file", err)
+		return
+	}
+
+	fmt.Printf("Line: %d\n", count)
+	fmt.Printf("Character: %d\n", countChar)
+	fmt.Printf("Word: %d\n", countStr)
+}
+
+//func countcharAndStr(a, b, c string) int {
+//
+//	for range a{
+//		a++
+//	}
+//	for range b{
+//		b++
+//	}
+//
+//}
